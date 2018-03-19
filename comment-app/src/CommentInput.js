c765169d9fae092c25f5457d6d8f9911cba0e779
yubl <yubl@c-bim.net>
2018-03-19 19:53:34 +0800

    add others

--- comment-app/src/CommentInput.js
+++ comment-app/src/CommentInput.js
@@ -2,10 +2,12 @@ import React,{Component} from 'react'
 import PropTypes from 'prop-types'
 
 class CommentInput extends Component{
+	//static开头类属性
 	static propTypes = {
 		// comment:PropTypes.object.isRequired
 	};
 
+	//构造函数
 	constructor(){
 		super();
 		this.state = {
@@ -13,30 +15,19 @@ class CommentInput extends Component{
 			content:''
 		}
 	}
-	handleUsernameChange(e){
-		this.setState({
-			username:e.target.value
-		})
-	}
-	handleContentChange(e){
-		this.setState({
-			content:e.target.value
-		})
-	}
-	handleButtonClick(e){
-		if(this.props.onSubmit){
-			const {username,content} = this.state;
-			this.props.onSubmit({username,content});
-			this.setState({
-				content:''
-			})
-		}
+
+	//TODO getter and setter
+
+	//组件生命周期
+	componentWillMount(){
+		this._loadUsername();
 	}
 
-	handleUsernameBlur(e){
-		this._saveUsername(e.target.value);
+	componentDidMount(){
+		this.textarea.focus();
 	}
 
+	//_私有方法
 	_saveUsername(username){
 		localStorage.setItem('username',username);
 	}
@@ -50,14 +41,36 @@ class CommentInput extends Component{
 		}
 	}
 
-	componentWillMount(){
-		this._loadUsername();
+
+	//时间监听方法
+	handleUsernameChange(e){
+		this.setState({
+			username:e.target.value
+		})
 	}
 
-	componentDidMount(){
-		this.textarea.focus();
+	handleContentChange(e){
+		this.setState({
+			content:e.target.value
+		})
 	}
 
+	handleButtonClick(e){
+		if(this.props.onSubmit){
+			const {username,content} = this.state;
+			const timeStamp = +new Date();
+			this.props.onSubmit({username,content,timeStamp});
+			this.setState({
+				content:''
+			})
+		}
+	}
+
+	handleUsernameBlur(e){
+		this._saveUsername(e.target.value);
+	}
+
+	//render
 	render(){
 		return (
 			<div className='comment-input'>
