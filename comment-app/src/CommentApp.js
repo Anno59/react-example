e96fb1941ce971a44187da290c558d45159d9bb4
yubl <yubl@c-bim.net>
2018-12-03 09:32:47 +0800

    更新至评论4

--- comment-app/src/CommentApp.js
+++ comment-app/src/CommentApp.js
@@ -9,18 +9,22 @@ class CommentApp extends Component{
     constructor(){
       super();
       this.state = {
-        comment: []
+        comment: localStorage.getItem('comment') || []
       }
     }
 
     handleInputComment(comment){
-			if (!comment.username) return alert('请输入用户名');
-			if (!comment.content) return alert('请输入评论内容');
-			this.state.comment.push(comment);
-      this.setState({
-				comment :this.state.comment
-      })
+        if (!comment.username) return alert('请输入用户名');
+        if (!comment.content) return alert('请输入评论内容');
+		let comment = this.state.comment.push(comment);
+		localStorage.setItem('comment',comment);
+        this.setState({
+          comment :this.state.comment
+        })
     }
+
+
+
     render(){
         return(
             <div className="wrapper">
