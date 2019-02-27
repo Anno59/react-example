ef7d63c12747dea8fd901506a894f282f0504fb9
yubl <yubl@c-bim.net>
2019-02-27 17:58:53 +0800

    添加webpack中plugin及loader的配置

--- webpack-sample-project/webpack.config.js
+++ webpack-sample-project/webpack.config.js
@@ -1,6 +1,9 @@
 /**
  * Created by 372025284@qq.com on 2019.02.24.
  */
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
     devtool : 'eval-source-map',
     entry:__dirname + '/app/main.js',
@@ -23,12 +26,22 @@ module.exports = {
                         loader:'style-loader'
                     },{
                         loader:'css-loader',
-                    options:{
-                        modules:true,
-                        localIdentName:'[name]__[local]--[hash:base64:5]'
+                        options:{
+                            modules:true,
+                            localIdentName:'[name]__[local]--[hash:base64:5]'
+                        }
+                    },{
+                        loader: 'postcss-loader'
                     }
-                }]
+                ]
             }
         ]
-    }
+    },
+    plugins:[
+        new webpack.BannerPlugin('测试版权'),
+        // new HtmlWebpackPlugin({
+        //     template:__dirname + '/app/index.tmpl.html'
+        // })
+    ]
+
 };
