570ca8d9de173569637efc438b3792855c16f5e7
yubl <yubl@c-bim.net>
2019-03-01 20:45:54 +0800

    添加webpack-learn

---
+++ learn-webpack/webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+    entry: './main.js',
+    output: {
+        filename:'bundle.js',
+        path: path.resolve(__dirname,'./dist')
+    },
+    module:{
+        rules:[{
+            test:/\.css$/,
+            use:[{
+                loader:'style-loader',
+            },{
+                loader:'css-loader',
+                options:{
+                    minimize:true
+                }
+            }]
+        }]
+    }
+}
